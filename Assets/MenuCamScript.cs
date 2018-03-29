9b325055b53df728ee517feec7e108ed6cbd6811
Jill Nesbit <scrunguscrungus@gmail.com>
2018-03-29 17:19:33 +0100

    Initial commit!
    Signed-off-by: Jill Nesbit <scrunguscrungus@gmail.com>

---
+++ Assets/MenuCamScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCamScript : MonoBehaviour {
+
+	private GameObject TargetPosition;
+	[SerializeField] public GameObject[] PossiblePositions;
+	[SerializeField] public int CurrentPosition;
+	private float PosSpeed;
+	private Vector3 tempPos;
+	private Quaternion tempRot;
+	// Use this for initialization
+	void Start () {
+
+		PosSpeed = 0.05f;
+
+		CurrentPosition = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TargetPosition = PossiblePositions [CurrentPosition];
+
+		if (TargetPosition != null) {
+			tempPos = transform.position;
+			tempPos.x += (TargetPosition.transform.position.x - transform.position.x) * PosSpeed;
+			tempPos.y += (TargetPosition.transform.position.y - transform.position.y) * PosSpeed;
+			tempPos.z += (TargetPosition.transform.position.z - transform.position.z) * PosSpeed;
+			transform.position = tempPos;
+
+			tempRot = transform.rotation;
+			tempRot.x += Mathf.Sin (TargetPosition.transform.rotation.x - transform.rotation.x) * PosSpeed;
+			tempRot.y += Mathf.Sin (TargetPosition.transform.rotation.y - transform.rotation.y) * PosSpeed;
+			tempRot.z += Mathf.Sin (TargetPosition.transform.rotation.z - transform.rotation.z) * PosSpeed;
+			transform.rotation = tempRot;
+		}
+		if (SceneManager.GetActiveScene ().name != "Menu_Network") {
+			DestroyObject (this);
+		}
+
+
+	}
+}
---
+++ Assets/TempSettings.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+public class TempSettings : NetworkBehaviour {
+
+
+	Rect MusVolSliderPos = new Rect(new Vector2((Screen.width/2)-400, (Screen.height/2)-10), new Vector2(100, 100));
+	Rect MusVolSliderLabel1Pos = new Rect(new Vector2((Screen.width/2)-400, Screen.height/2), new Vector2(100, 100));
+	Rect MusVolSliderLabel2Pos = new Rect(new Vector2((Screen.width/2)-300, Screen.height/2), new Vector2(100, 100));
+	Rect SfxVolSliderPos = new Rect(new Vector2((Screen.width/2)-400, (Screen.height/2)-100), new Vector2(100, 100));
+	Rect SfxVolSliderLabel1Pos = new Rect(new Vector2((Screen.width/2)-400, (Screen.height/2)-100), new Vector2(100, 100));
+	Rect SfxVolSliderLabel2Pos = new Rect(new Vector2((Screen.width/2)-300, (Screen.height/2)-100), new Vector2(100, 100));
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (SceneManager.GetActiveScene().name != "Menu_Network") {
+			Destroy (this);
+		}
+	}
+
+	void OnGUI() {
+		float VolSlider = GUI.HorizontalSlider (MusVolSliderPos, PlayerPrefs.GetFloat("volume"), 0.0f, 1.0f);
+		PlayerPrefs.SetFloat ("volume", VolSlider);
+		float SfxSlider = GUI.HorizontalSlider (SfxVolSliderPos, PlayerPrefs.GetFloat ("sfxvolume"), 0.0f, 1.0f);
+		PlayerPrefs.SetFloat ("sfxvolume", SfxSlider);
+
+		GUI.Label (MusVolSliderLabel1Pos, "Music Volume: ");
+		GUI.Label(MusVolSliderLabel2Pos, System.Math.Round(VolSlider, 2).ToString());
+
+		GUI.Label (SfxVolSliderLabel1Pos, "SFX Volume: ");
+		GUI.Label (SfxVolSliderLabel2Pos, System.Math.Round (SfxSlider, 2).ToString ());
+
+	}
+}
